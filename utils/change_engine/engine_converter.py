c4e51bec707b482f951c1a73d37634cb34e2ef87
Cabpacob <Cabpacob@github.com>
2021-12-06 17:38:29 +0300

    Uniqued converting

---
+++ utils/change_engine/engine_converter.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+from clickhouse_driver import Client
+from interface import Database
+import sys
+from enum import Enum
+
+
+class UserInteractor:
+    def ask(self, text):
+        answer = input(text)
+        return answer
+
+    def notify(self, text): #TODO logging
+        print(text)
+
+
+class Action(Enum):
+    CONTINUE = 1
+    REVERT = 2
+    CONVERT = 3
+    TERMINATE = 4
+
+
+class DBEngineConverter:
+    def __init__(self):
+        self.__temporary_prefix = None
+        self.__client = Client('localhost')
+        self.__user_interactor = UserInteractor()
+
+    def __get_temporary_name(self, database_name):
+        assert self.__temporary_prefix is not None
+        return f'{self.__temporary_prefix}{database_name}'
+
+    def __get_needed_action(self, database, temp_database):
+        if temp_database.exists:
+            answer = self.__user_interactor.ask(
+                f'Changing for database {database.name} failed in previous launch.\n'
+                 'Do you want to continue changing? (y/n)\n'
+            )
+
+            if answer == 'y':
+                return Action.CONTINUE
+            elif answer == 'n':
+                return Action.REVERT
+            else:
+                self.__user_interactor.notify(f'Wrong answer, skipped')
+                return Action.TERMINATE
+        else:
+            return Action.CONVERT
+
+    def convert(self, database_name, engine_from, engine_to): #TODO engine name validation
+        self.__temporary_prefix = f'__temporary_{engine_from}_to_{engine_to}__'
+
+        database = Database(database_name, self.__client)
+        temp_database = Database(self.__get_temporary_name(database_name), self.__client)
+
+        action = self.__get_needed_action(database, temp_database)
+
+        if action == Action.CONVERT or action == Action.CONTINUE:
+            if not database.exists:
+                raise ValueError(f'database \'{database.name}\' does not exists')
+
+            if database.engine != engine_from:
+                raise ValueError(f'database {database.name} engine must be {engine_from}')
+
+            if not temp_database.exists:
+                temp_database.create(engine=engine_to)
+
+            temp_database.move_tables(database.tables)
+            database.drop()
+
+            if engine_to == 'Atomic':
+                temp_database.rename(database_name)
+            else:
+                database.create(engine=engine_to)
+                database.move_tables(temp_database.tables)
+                temp_database.drop()
+
+            assert database.engine == engine_to, 'something went wrong'
+        elif action == Action.REVERT:
+            if not ordinary_database.exists:
+                database.create(engine=engine_from)
+
+            database.move_tables(temp_database.tables)
+            temp_database.drop()
+        elif action == Action.TERMINATE:
+            pass
+
+
+def main():
+    converter = DBEngineConverter()
+
+    db, engine_from, engine_to = sys.argv[1:4]
+    converter.convert(db, engine_from, engine_to)
+
+
+if __name__ == '__main__':
+    main()
